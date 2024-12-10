9c45ef472367f709d4b6612916058bc54e772e94
Ruzzel <120955455+WindWalker01@users.noreply.github.com>
2024-12-10 21:45:38 +0800

    incident

--- src/main/java/org/example/criticclick/CreateController.java
+++ src/main/java/org/example/criticclick/CreateController.java
@@ -1,5 +1,15 @@
 package org.example.criticclick;
 
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class CreateController extends SceneController {
+
+
+    public void onChangeToHome(ActionEvent event) throws IOException {
+        changeScene("Home", event);
+    }
 
 }
--- src/main/java/org/example/criticclick/HomeController.java
+++ src/main/java/org/example/criticclick/HomeController.java
@@ -1,4 +1,34 @@
 package org.example.criticclick;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+
 public class HomeController  extends SceneController{
+
+    @FXML
+    private HBox popularFilms;
+
+    @FXML
+    public void initialize() {
+        try {
+            for (int i = 0; i < 15; i++){
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("MovieCard.fxml"));
+                Node movie = loader.load();
+                MovieCardController controller = loader.getController();
+
+                controller.setTitle("Movie");
+                controller.setYear("year");
+                controller.setPoster("/yh64qw9mgXBvlaWDi7Q9tpUBAvH.jpg");
+
+                popularFilms.getChildren().add(movie);
+
+
+            }
+
+        }catch (Exception e) {
+
+        }
+    }
 }
