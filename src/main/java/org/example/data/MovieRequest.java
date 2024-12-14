219da58d908bb1cce4618ee32a39f92445eb4f2f
Ruzzel <120955455+WindWalker01@users.noreply.github.com>
2024-12-15 07:00:30 +0800

    add api request for searched names

--- src/main/java/org/example/data/MovieRequest.java
+++ src/main/java/org/example/data/MovieRequest.java
@@ -13,19 +13,21 @@ import java.util.ArrayList;
 
 public class MovieRequest {
 
-    private void getMovieByName(String query){
+    private ArrayList<Movie> getMoviesByName(String query){
         try{
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.themoviedb.org/3/search/movie?query=" + query.replaceAll("\\s", "") + "&include_adult=false&language=en-US&page=1"))
+                    .uri(URI.create("https://api.themoviedb.org/3/search/movie?query=" + query + "&include_adult=false&language=en-US&page=1"))
                     .header("accept", "application/json")
                     .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYmY5Nzg2NjQ0ODQ1NjIyMDBjZGUwMzVmN2JhNzFhZiIsIm5iZiI6MTczMzM4MDg1My4zMDgsInN1YiI6IjY3NTE0YWY1NzJkMWE4YTU5YzI5NzU3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VVMB6g66OlAQbOz1SlYVUTPuG4xl74PKdJ2-hqic1zE")
                     .method("GET", HttpRequest.BodyPublishers.noBody())
                     .build();
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+
         }catch (Exception e){
             System.err.println("Something went wrong: API request declined or something idk");
         }
+
+        return null;
     }
 
     // Now Playing
