8abaa4bc1033c3b7729aa1517d94cf890eef21f0
Kyle <kmccsabado23@bsu.edu.ph>
2024-12-14 21:12:30 +0800

    yung nawawalala

--- src/main/java/org/example/CriticWindow.java
+++ src/main/java/org/example/CriticWindow.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.components.TitleBar;
 import org.example.dsa.HashMapngGroup1;
 import org.example.pages.*;
 
@@ -11,11 +12,13 @@ public class CriticWindow extends JFrame {
     public static final Color LIGHT_BEIGE = new Color(229, 225, 218);
     private final int WINDOW_WIDTH = 1280;
     private final int WINDOW_HEIGHT = 720;
+    private TitleBar titleBar;
 
 
     private HashMapngGroup1<Page, CriticPage> pages = new HashMapngGroup1();
 
     public CriticWindow(){
+
         setLayout(null);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setLocationRelativeTo(null);
@@ -23,6 +26,10 @@ public class CriticWindow extends JFrame {
         setVisible(true);
         setTitle("CriticClick");
         setResizable(false);
+
+        titleBar = new TitleBar();
+        titleBar.setWindow(this); // Pass CriticWindow reference to TitleBar
+        add(titleBar, BorderLayout.NORTH);
 
         addPage(Page.Home, new Home(this));
         addPage(Page.Create, new Create(this));
@@ -32,7 +39,7 @@ public class CriticWindow extends JFrame {
         changePage(Page.Profile);
 
     }
-
+    
 
     public void addPage(Page type, CriticPage panel){
         pages.set(type, panel);
@@ -43,6 +50,9 @@ public class CriticWindow extends JFrame {
 
 
     public void changePage(Page desiredPage){
+        if(desiredPage == Page.MainPage){
+            remove(titleBar);
+        }
         for (HashMapngGroup1.Node<Page, CriticPage> set : pages.entrySet()){
             if(set.key == desiredPage){
                 set.value.setVisible(true);
